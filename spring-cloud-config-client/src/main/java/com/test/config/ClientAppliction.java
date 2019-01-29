adfa9d85b0c8ae9b187364fef0615ceda4c06aab
FF0504 <2993394628@qq.com>
2019-01-29 14:23:53 +0800

    Initial commit

---
+++ spring-cloud-config-client/src/main/java/com/test/config/ClientAppliction.java
@@ -0,0 +1,15 @@
+package com.test.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: FF
+ * @Date: 2019/1/28 17:41
+ */
+@SpringBootApplication
+public class ClientAppliction {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientAppliction.class, args);
+    }
+}
---
+++ spring-cloud-config-service/src/main/java/com/test/config/ServiceApplication.java
@@ -0,0 +1,17 @@
+package com.test.config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: FF
+ * @Date: 2019/1/28 17:07
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
